c6ef405826b82f8631174bcdf00647fdddba13fa
IvanSanchezDev <ivancampus2023@gmail.com>
2023-08-18 09:11:59 -0500

    feat: :sparkles: middleware de creacion del token para cada coleccion

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { appToken } from './helpers/jwt.js'
 
 dotenv.config()
 
-
-const app=express()
-
+const app = express()
 
 app.use(express.json())
 
+app.use('/api/token', appToken)
 
-const port=1234 ?? process.env.PORT
+const port = 1234 ?? process.env.PORT
 
-
-app.listen(port, ()=>{
-    console.log(`server listening in http://localhost:${port}/`);
+app.listen(port, () => {
+  console.log(`server listening in http://localhost:${port}`)
 })
